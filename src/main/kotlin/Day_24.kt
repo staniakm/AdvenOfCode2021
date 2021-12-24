e768ead1d012650b2949e8b5648a27887e1f8cd7
Mariusz Staniak <mariusz.staniak@gmail.com>
2021-12-24 12:46:58 +0100

    day 24

---
+++ src/main/kotlin/Day_24.kt
@@ -0,0 +1,27 @@
+fun run(part2: Boolean): Any {
+    val blocks = readFileFromResources("day24_data").chunked(18)
+    val result = MutableList(14) { -1 }
+    val buffer = ArrayDeque<Pair<Int, Int>>()
+    fun List<String>.lastOf(command: String) = last { it.startsWith(command) }.split(" ").last().toInt()
+    val best = if (part2) 1 else 9
+    blocks.forEachIndexed { index, instructions ->
+        if ("div z 26" in instructions) {
+            val offset = instructions.lastOf("add x")
+            val (lastIndex, lastOffset) = buffer.removeFirst()
+            val difference = offset + lastOffset
+            if (difference >= 0) {
+                result[lastIndex] = if (part2) best else best - difference
+                result[index] = if (part2) best + difference else best
+            } else {
+                result[lastIndex] = if (part2) best - difference else best
+                result[index] = if (part2) best else best + difference
+            }
+        } else buffer.addFirst(index to instructions.lastOf("add y"))
+    }
+
+    return result.joinToString("").toLong()
+}
+
+fun main() {
+    println(run(true))
+}
